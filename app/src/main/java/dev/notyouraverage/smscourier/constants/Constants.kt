cc064ec903d6cf35ac15ce5d592e6ed096886d7c
A Anand <a.anand.91119@gmail.com>
2026-01-19 10:48:03 +0530

    feat(13-03): add About URLs to Constants
    - Add AboutLinks object with PRIVACY_POLICY_URL and SUPPORT_URL
    - URLs point to GitLab repository (placeholder for privacy, issues for support)
    - Externalizes URLs from UI code for easier maintenance

--- app/src/main/java/dev/notyouraverage/smscourier/constants/Constants.kt
+++ app/src/main/java/dev/notyouraverage/smscourier/constants/Constants.kt
@@ -16,3 +16,9 @@ object Constants {
     const val NOTIFICATION_CHANNEL_GENERAL = "ListeningNotificationChannel"
     const val CODE_FOREGROUND_SERVICE = 1
 }
+
+object AboutLinks {
+    // Privacy policy placeholder - update when real URL is available
+    const val PRIVACY_POLICY_URL = "https://gitlab.com/notyouraverage/smscourier/-/blob/main/PRIVACY.md"
+    const val SUPPORT_URL = "https://gitlab.com/notyouraverage/smscourier/-/issues"
+}
